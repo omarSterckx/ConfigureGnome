43654da87a5572db30c2144317eec7c800f889b1
omarSterckx <omar.Sterckx@hotmail.com>
2017-11-16 13:47:25 +0100

    Added power optimizer

--- ConfigureGnome.sh
+++ ConfigureGnome.sh
@@ -6,6 +6,9 @@
 
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
 
+#install power optimizer
+sudo apt install tlp tlp-rdw
+
 # Collect and install must have apps
 sudo add-apt-repository -y ppa:nilarimogard/webupd8
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
